e3478f3bbbb60add762fcaf2e84afd20dfb41351
Niraj Bajracharya <niraj.bajracharya@tekvortex.com>
2024-03-27 16:31:44 +0545

    improved folder structure

--- backup/entrypoint.sh
+++ backup/entrypoint.sh
@@ -7,8 +7,11 @@ mount --verbose -t cifs $SMB_NETWORK_PATH $SMB_MOUNT_PATH -o username=$SMB_USERN
 # dump mysql database
 mysqldump -h orangehrm-mariadb --user=$MARIADB_USERNAME --password=$MARIADB_PASSWORD orangehrm > /orangehrm-bkp-$DATETIME.sql
 
-# compress my sql dump
-tar -czvf orangehrm-bkp-$DATETIME.tar.gz /orangehrm-bkp-$DATETIME.sql
+if [ $? -eq 0 ]
+    then 
+        # compress my sql dump
+        tar -czvf orangehrm-bkp-$DATETIME.tar.gz /orangehrm-bkp-$DATETIME.sql
 
-# copy tar.gz to smb server
-cp orangehrm-bkp-$DATETIME.tar.gz $SMB_MOUNT_PATH
+        # copy tar.gz to smb server
+        cp orangehrm-bkp-$DATETIME.tar.gz $SMB_MOUNT_PATH
+fi
