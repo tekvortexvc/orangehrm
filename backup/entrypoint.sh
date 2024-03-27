b002b022900a57745db2962545b7dfbd7b8849fa
Niraj Bajracharya <niraj.bajracharya@tekvortex.com>
2024-03-27 16:37:02 +0545

    removed redundant env var

--- backup/entrypoint.sh
+++ backup/entrypoint.sh
@@ -5,7 +5,7 @@ DATETIME=$(date '+%Y%m%d-%H%M%S')
 mount --verbose -t cifs $SMB_NETWORK_PATH $SMB_MOUNT_PATH -o username=$SMB_USERNAME,password=$SMB_PASSWORD
 
 # dump mysql database
-mysqldump -h orangehrm-mariadb --user=$MARIADB_USERNAME --password=$MARIADB_PASSWORD orangehrm > /orangehrm-bkp-$DATETIME.sql
+mysqldump -h orangehrm-mariadb --user=$MARIADB_USERNAME --password=$MYSQL_ROOT_PASSWORD orangehrm > /orangehrm-bkp-$DATETIME.sql
 
 if [ $? -eq 0 ]
     then 
